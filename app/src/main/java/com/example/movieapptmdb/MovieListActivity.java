5dd379f6ec50df65bef2728d72c33353c2246029
FredAndrade <freedandrade@gmail.com>
2021-08-16 21:56:11 -0400

    Melhoria nas Views

--- app/src/main/java/com/example/movieapptmdb/MovieListActivity.java
+++ app/src/main/java/com/example/movieapptmdb/MovieListActivity.java
@@ -11,11 +11,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Movie;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.movieapptmdb.adapters.MovieRecyclerView;
